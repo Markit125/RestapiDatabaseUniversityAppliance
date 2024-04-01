eeae84438c64ffb1039f33c1defc86d38fce7d52
Markit125 <tamarkus0@gmail.com>
2024-04-02 02:30:28 +0300

    migrations

--- migrations/20240401203648_create_students.up.sql
+++ migrations/20240401203648_create_students.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE students
 (
-  student_id           INTEGER NOT NULL PRIMARY KEY,
-  student_first_name   VARCHAR(20) NOT NULL,
-  student_middle_name  VARCHAR(20) NOT NULL,
-  student_last_name    VARCHAR(20) NOT NULL,
+  id           INTEGER NOT NULL PRIMARY KEY,
+  first_name   VARCHAR(20) NOT NULL,
+  middle_name  VARCHAR(20) NOT NULL,
+  last_name    VARCHAR(20) NOT NULL,
   birth_date           DATE NOT NULL,
   achievements         INTEGER NOT NULL DEFAULT 0,
   passport             VARCHAR(20) NOT NULL UNIQUE
